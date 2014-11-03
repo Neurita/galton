9c1a33c668b910124f315520dfcbcafba06efbc7
Alexandre M. S <alexsavio@gmail.com>
2014-11-03 15:37:59 +0100

    Add templatized logger config

--- galton/logger.py
+++ galton/logger.py
@@ -1,30 +1,31 @@
-
+# -*- coding: utf-8 -*-
 import os
+import os.path as op
 import yaml
 import logging.config
 
-from .config import LOG_LEVEL
+from .text_files import read
+from ..config import LOG_LEVEL
+
+MODULE_NAME = __name__.split('.')[0]
 
 
-def setup_logging(default_path=os.path.join(os.path.dirname(__file__),
-                                            'logger.yaml'),
-                  default_level=LOG_LEVEL,
-                  env_key='GALTON_LOG_CFG'):
-    """Setup logging configuration
-
-    """
-    path = default_path
+def setup_logging(log_config_file=op.join(op.dirname(__file__), 'logger.yml'),
+                  log_default_level=LOG_LEVEL,
+                  env_key=MODULE_NAME + '_LOG_CFG'):
+    """Setup logging configuration."""
+    path = log_config_file
     value = os.getenv(env_key, None)
     if value:
         path = value
-    if os.path.exists(path):
-        with open(path, 'rt') as f:
-            config = yaml.load(f)
-        logging.config.dictConfig(config)
+
+    if op.exists(path):
+        log_cfg = yaml.load(read(path).format(MODULE_NAME))
+        logging.config.dictConfig(log_cfg)
         #print('Started logging using config file {0}.'.format(path))
     else:
-        logging.basicConfig(level=default_level)
+        logging.basicConfig(level=log_default_level)
         #print('Started default logging. Could not find config file '
         #      'in {0}.'.format(path))
     log = logging.getLogger(__name__)
-    log.info('Start logging.')
+    log.debug('Start logging.')
